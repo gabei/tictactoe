f1f01994975592f60f34e5f533f4f2d21f4e3a00
gabei <gabe1792@gmail.com>
2021-11-20 16:51:10 -0600

    added player name entry to script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,50 @@
 (() => {
   console.log("Hello.");
+
+  (function Board() {
+    let spaces = [];
+
+    function updateBoard(space) {
+      spaces.push(space);
+    }
+
+    function clearBoard() {
+      spaces = [];
+    }
+
+    return { updateBoard, clearBoard };
+  })();
+
+  (function Control() {
+    let isGameOver;
+    let currentPlayer;
+
+    function getPlayerName(playerSign) {
+      let name = alert(`What is ${playerSign}'s name?`);
+      return name;
+    }
+
+    function addPlayers() {
+      let playerX = getPlayerName("x");
+      let playerO = getPlayerName("o");
+    }
+
+    function setupGame() {}
+
+    function playGame() {}
+
+    function nextPlayer() {
+      currentPlayer.sign = "x" ? (currentPlayer = "o") : (currentPlayer = "x");
+    }
+  })();
+
+  function Player(name, playerSign) {
+    let spaces = [];
+    let sign = playerSign;
+    function addSpace(space) {
+      spaces.push(space);
+    }
+
+    return { name, sign, addSpace };
+  }
 })();
