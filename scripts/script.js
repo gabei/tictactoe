1c97f6696f61b7ea67b3973a3dc91b262372ebd4
gabei <gabe1792@gmail.com>
2021-11-22 16:08:44 -0600

    added winning combos array to controls

--- scripts/script.js
+++ scripts/script.js
@@ -21,6 +21,17 @@
     let playerX;
     let playerO;
 
+    let winningCombos = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [7, 5, 3],
+    ];
+
     function getPlayerName(playerSign) {
       let name = prompt(`What is ${playerSign}'s name?`);
       return name;
