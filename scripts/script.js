690a171ba1fc7efee33ae1e6566e4f94db92c153
gabei <gabe1792@gmail.com>
2021-12-01 17:23:41 -0600

    added comments for each module. Its a little easier to look at now

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,11 @@
 (() => {
-  const Board = (function () {
-    let spaces = [];
+  /*__________________________________________________ */
 
-    function getSpaces() {
-      return spaces;
-    }
-
-    function updateBoard(space) {
-      spaces.push(space);
-    }
-
-    function clearBoard() {
-      spaces = [];
-    }
-
-    return { updateBoard, clearBoard, getSpaces };
-  })();
+  /* Control
+  - Maintains the flow of the game's logic
+  - Calls player and board methods to update their status
+  - Harbors the functions that are attached to DOM elements in the UI
+  ____________________________________________________ */
 
   const Control = (function () {
     let currentPlayer;
@@ -42,6 +32,7 @@
 
     function setupGame(e) {
       e.preventDefault();
+
       let xname = this.elements["x-name"].value;
       let yname = this.elements["y-name"].value;
       addPlayers(xname, yname);
@@ -57,55 +48,17 @@
       UI.changeTurnText(currentPlayer.getName());
     }
 
-    function resetGame() {
-      UI.hideGameOver();
-      UI.initGameDisplay();
-      Board.clearBoard();
-    }
-
-    function isValid(choice) {
-      let isAvailable = !Board.getSpaces().includes(choice);
-      console.log(isAvailable);
-      return isAvailable;
-    }
-
-    function nextPlayer() {
-      currentPlayer == playerX
-        ? (currentPlayer = playerO)
-        : (currentPlayer = playerX);
-
-      UI.changeTurnText(currentPlayer.getName());
-    }
-
-    function isWinningMove() {
-      let win = false;
-      let playerSpaces = currentPlayer.getSpaces();
-      if (playerSpaces.length < 3) return win;
-
-      // does player have all nums of a winning combo
-      winningCombos.forEach(function (combo) {
-        let comboCount = 0;
-
-        for (let num of combo) {
-          if (playerSpaces.includes(num)) {
-            comboCount++;
-          }
-
-          if (comboCount === 3) {
-            win = true;
-            break;
-          }
-        }
-      });
-
-      return win;
-    }
-
     function endGame(turncount, draw = false) {
       UI.disableButtons();
       draw
         ? UI.showGameOver(turncount, "Nobody")
         : UI.showGameOver(turncount, currentPlayer.getName());
+    }
+
+    function resetGame() {
+      UI.hideGameOver();
+      UI.initGameDisplay();
+      Board.clearBoard();
     }
 
     function playTurn(e) {
@@ -125,8 +78,84 @@
       }
     }
 
+    function nextPlayer() {
+      currentPlayer == playerX
+        ? (currentPlayer = playerO)
+        : (currentPlayer = playerX);
+
+      UI.changeTurnText(currentPlayer.getName());
+    }
+
+    function isValid(choice) {
+      let isAvailable = !Board.getSpaces().includes(choice);
+      console.log(isAvailable);
+      return isAvailable;
+    }
+
+    function isWinningMove() {
+      let isWin = false;
+      let playerSpaces = currentPlayer.getSpaces();
+      if (playerSpaces.length < 3) return isWin;
+
+      // does player have all nums of a winning combo
+      winningCombos.forEach(function (combo) {
+        let comboCount = 0;
+
+        for (let num of combo) {
+          if (playerSpaces.includes(num)) {
+            comboCount++;
+          }
+
+          if (comboCount === 3) {
+            isWin = true;
+            break;
+          }
+        }
+      });
+
+      return isWin;
+    }
+
     return { setupGame, playTurn, endGame, resetGame };
   })();
+
+  /*__________________________________________________ */
+  /* Board
+
+  - Tracks all currently used spaces
+  - Spaces are numbered 1-9 according to a tic-tac-toe grid:
+      1 | 2 | 3
+      __________
+      4 | 5 | 6
+      __________
+      7 | 8 | 9
+  ____________________________________________________ */
+
+  const Board = (function () {
+    let spaces = [];
+
+    function getSpaces() {
+      return spaces;
+    }
+
+    function updateBoard(space) {
+      spaces.push(space);
+    }
+
+    function clearBoard() {
+      spaces = [];
+    }
+
+    return { getSpaces, updateBoard, clearBoard };
+  })();
+
+  /*__________________________________________________ */
+  /* Player
+
+  - Holds player names
+  - Holds player symbol (currently just 'x' or 'o')
+  - Tracks player's held spaces according to board grid (1-9)
+  ____________________________________________________ */
 
   function Player(name, playerSign) {
     let spaces = [];
@@ -151,6 +180,13 @@
     return { getName, getSign, addSpace, getSpaces };
   }
 
+  /*__________________________________________________ */
+  /* UI
+
+  - Sets up DOM elements for display and player control
+  - Event listeners here hold callback functions from the Control module
+  - The Control methods invoke the public UI functions
+  ____________________________________________________ */
   const UI = (function () {
     const textUpdate = document.querySelector(".text-updates");
     const turnText = document.querySelector(".turn-text");
@@ -161,6 +197,7 @@
     const gameSetup = document.querySelector(".game-setup");
     const setupForm = document.querySelector(".game-setup__form");
     const reset = document.querySelector(".reset");
+
     gameBoard.addEventListener("click", Control.playTurn);
     setupForm.addEventListener("submit", Control.setupGame);
     reset.addEventListener("click", Control.resetGame);
